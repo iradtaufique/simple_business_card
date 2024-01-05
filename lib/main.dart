e5cf7ed7c98d9d89d1a13fcb3e8bece86a5b1a3e
Taufique Iradukunda <taufiqueira@Taufiques-MacBook-Pro.local>
2024-01-04 22:22:00 -0500

    added a stateless(stless for shortcut) widget that will help in hot-reload and hot-restart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+// this MyApp is a stateless Widget I created that will help in hot-reload and hot-restart of the project
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.amber,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
