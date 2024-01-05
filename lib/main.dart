35ab6693550c539521dd479ddc4194847758b0a0
Taufique Iradukunda <taufiqueira@Taufiques-MacBook-Pro.local>
2024-01-04 23:12:07 -0500

    give my container some margin, height, padding, and width

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.amber,
-        body: Container(),
+        // SafeAre widget is used to avoid element in container to be on the edge of the screen
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            // margin give space on the outside
+            margin: EdgeInsets.all(30.0),
+            padding: EdgeInsets.fromLTRB(20, 50, 20, 50),
+            height: 150,
+            width: 150,
+            child: Text("Container Element"),
+          ),
+        ),
       ),
     );
   }
